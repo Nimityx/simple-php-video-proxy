21af69d83a2be470706c2134ac16fd49d0b1f2e0
Nimityx <54273067+Nimityx@users.noreply.github.com>
2020-11-05 15:36:40 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+if (isset($_GET['url'])) {
+    if ($_GET['url']==null || !$_GET['url']) {
+        echo "URL undefined!";
+    } else {
+        $url = $_GET['url'];
+        $url = $_SERVER['HTTP_HOST'];."/server.php?url=".urlencode($url);
+        echo '
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="robots" content="none" />
+        <meta name="googlebot" content="none" />
+        <link rel="icon" type="image/png" href="https://edpuzzle.imgix.net/favicons/favicon-32.png">
+        <title>Edpuzzle</title>
+    </head>
+    <body bgcolor="#222222">
+        <video controls autoplay src="'.$url.'" style="position:fixed;width:100%;height:100%;top:0;bottom:0;left:0;right:0;">Your browser does not support the video tag.</video>
+    </body>
+</html>';
+    }
+} else {
+    echo "URL undefined!";
+}
+
+?>
