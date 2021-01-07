a71e983b15774b8d9d27a2d9a4f873edcbb2dc9f
Nimityx <c93c81b9-b581-4651-89fc-a416fd69558e@anonaddy.me>
2021-01-07 22:07:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if (isset($_GET['url'])) {
 <html>
     <head>
         <title>PHP Video Proxy</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=1" />
     </head>
     <body>
         <video controls autoplay src="'.$url.'" style="position:fixed;width:100%;height:100%;top:0;bottom:0;left:0;right:0;">Your browser does not support the video tag.</video>
